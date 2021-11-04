06bcb45d871b2ea66b71a0845a8d20e176a86323
uliadib12 <uliadib@gmail.com>
2021-11-04 23:30:54 +0700

    fix select menu

--- src/comp/SideBar.js
+++ src/comp/SideBar.js
@@ -14,9 +14,9 @@ export function Sidebar(props) {
     return (
         <>
             <div className="flex flex-col border-2 left-0 h-full fixed justify-center">
-                <Link to="/"><Menubar text={"Home"} className={border.home} onClick={()=>{onclickHandler("home")}}/></Link>
-                <Link to="/users"><Menubar text={"User"} className={border.user} onClick={()=>{onclickHandler("user")}}/></Link>
-                <Link to="/about"><Menubar text={"About Us"} className={border.aboutus} onClick={()=>{onclickHandler("aboutus")}}/></Link>
+                <Menubar to={"/"} text={"Home"} className={border.home} onClick={()=>{onclickHandler("home")}}/>
+                <Menubar to={"/users"} text={"User"} className={border.user} onClick={()=>{onclickHandler("user")}}/>
+                <Menubar to={"/about"} text={"About Us"} className={border.aboutus} onClick={()=>{onclickHandler("aboutus")}}/>
             </div>
         </>
     )
@@ -24,6 +24,6 @@ export function Sidebar(props) {
 
 function Menubar(props){
     return(
-        <div className={`flex items-center border-r-4 w-20 justify-center cursor-pointer mb-10 hover:border-blue-500 ${props.className ? "border-blue-500" : "border-gray-300"}`} onClick={props.onClick}>{props.text}</div>
+        <div className={`flex items-center border-r-4 w-20 justify-center cursor-pointer mb-10 hover:border-blue-500 ${props.className ? "border-blue-500" : "border-gray-300"}`} onClick={props.onClick}><Link to={props.to}>{props.text}</Link></div>
     )
 }
