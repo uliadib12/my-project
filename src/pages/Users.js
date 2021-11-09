d48b4df5ac67d0bc9fa6468317dbdec66d66a41f
uliadib12 <uliadib@gmail.com>
2021-11-09 22:30:43 +0700

    add Name, Connect twiter

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
 import '../body.css';
 import Sidebar from '../comp/SideBar';
 import { Topbar } from '../comp/Topbar';
+import {BsTwitter} from 'react-icons/bs';
 
 export function Users(props) {
 
@@ -15,7 +16,15 @@ export function Users(props) {
                       User Info
                   </h2>
                   <div style={{minHeight: "500px"}} className="bg-white border-2 mt-4 ml-7 p-3">
-              
+                    <div className="flex min-w-full justify-between items-center">
+                      <div className="ml-5 mt-5 font-medium text-2xl">
+                        Adib Ulinuha El Majid
+                        <div className="-mt-1 text-sm text-md font-medium cursor-pointer text-gray-400 hover:text-black">Edit your name,avatar,ect.</div>
+                      </div>
+                      <div className="group mr-6 border-2 hover:border-blue-400 rounded-md px-4 py-3 cursor-pointer">
+                        <div className="flex items-center text-gray-400 group-hover:text-blue-400"><BsTwitter className="mr-2 text-blue-600"/>Connect To Twitter</div>
+                      </div>
+                    </div>
                   </div>
               </div> 
           </div>
