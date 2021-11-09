33bf79ed8dcf71bd0abe2e59ef38d1693ba53c4c
uliadib12 <uliadib@gmail.com>
2021-11-09 22:05:55 +0700

    add user Info tamplate

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -9,8 +9,15 @@ export function Users(props) {
         <>
           <Sidebar/>
           <Topbar/>
-          <div className="body">
-            <h2>Users</h2>
+          <div className="bg-gray-100 pb-10 min-h-screen">
+            <div className="body">
+                  <h2 className="relative font-bold text-lg mt-7 ml-7">
+                      User Info
+                  </h2>
+                  <div style={{minHeight: "500px"}} className="bg-white border-2 mt-4 ml-7 p-3">
+              
+                  </div>
+              </div> 
           </div>
         </>
     )
