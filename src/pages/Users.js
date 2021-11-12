ffa2e555e35cb39581dc91ef9a671ba6c8777296
uliadib12 <uliadib@gmail.com>
2021-11-13 00:26:43 +0700

    fix avatar

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import '../body.css';
 import Sidebar from '../comp/SideBar';
 import { Topbar } from '../comp/Topbar';
@@ -8,6 +8,27 @@ import avatar from '../assets/image/avatar-placeholder.png'
 
 
 export function Users(props) {
+  const [file, setfile] = useState(null)
+  const [Error, setError] = useState({state:false, payload:""})
+  const hiddenFileInput = useRef()
+
+  const types= ["image/png", "image/jpeg"]
+
+  const handleClick = ()=>{
+      setError({state: false, payload:""})
+      hiddenFileInput.current.click();
+  }
+  
+  const handleChange = event=>{
+    const Inputfile = event.target.files[0]
+    if(Inputfile && types.includes(Inputfile.type)){
+      setfile(Inputfile)
+    }
+    else{
+      setfile(null)
+      setError({state: true, payload:"please select image"})
+    }
+  }
 
     return (
         <>
@@ -28,15 +49,21 @@ export function Users(props) {
                         <div className="flex items-center text-gray-400 group-hover:text-blue-400"><BsTwitter className="mr-2 text-blue-600"/>Connect To Twitter</div>
                       </div>
                     </div>
-                    <div className="mt-5 ml-5 grid grid-cols-3 gap-3">
+                    <div className="gridTamplate gap-1 mt-5 ml-5">
                       <div>
                         <div className="py-8 flex flex-col justify-center">
-                          <img className="w-56 mx-auto rounded-full" src={avatar} alt="avatar"/>
+                          <img className="w-56 mx-auto rounded-md" src={avatar} alt="avatar"/>
                           <div className="flex justify-center mt-8">
-                            <div className="group hover:border-blue-500 cursor-pointer border-2 border-gray-300 rounded-md py-3 px-14">
-                              <div className="flex items-center text-lg font-semibold group-hover:text-blue-500"><FaCamera className="mr-2"/>Upload</div>
-                            </div>
+                            <form>
+                              <div onClick={handleClick} className="group hover:border-blue-500 cursor-pointer border-2 border-gray-300 rounded-md py-3 px-14">
+                                  <div for="upload-file"  className="flex items-center text-lg font-semibold group-hover:text-blue-500"><FaCamera className="mr-2"/>
+                                    Upload
+                                  </div>
+                                  <input type="file" className="hidden" onChange={handleChange} ref={hiddenFileInput} />
+                              </div>
+                            </form>
                           </div>
+                            {Error.state && <div className="flex justify-center">{Error.payload}</div>}
                         </div>
                       </div>
                       <div>Test2</div>
